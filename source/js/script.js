4aff6626f8709d4a923b6a36a233c39d67e8f9df
Julia Prokhorova <julia.pro.smm@gmail.com>
2023-09-07 14:34:48 +0300

    Исправляет навигацию

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+//открывает-закрывает меню, но нет картинки бургера и крестика
+
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.nav__toggle');
 
@@ -6,3 +8,23 @@ if (navBtn) {
     nav.classList.toggle('is-open');
   })
 }
+
+
+
+//код из обучающего видео, не работает
+
+//let navMain = document.querySelector('.nav');
+//let navToggle = document.querySelector('nav__toggle');
+
+//navMain.classList.remove('nav--nojs');
+
+//navToggle.addEventListener('click', function () {
+ // if (navMain.classList.contains('nav__toggle--closed')) {
+ //   navMain.classList.remove('nav__toggle--closed');
+  //  navMain.classList.add('nav__toggle--opened');
+ // } else {
+ //   navMain.classList.add('nav__toggle--closed');
+//    navMain.classList.remove('nav__toggle--opened');
+//  }
+//}
+//)
