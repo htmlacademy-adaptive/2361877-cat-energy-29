b26b9a0798ea9e53bc6a5a1979ab890e3e558659
Julia Prokhorova <julia.pro.smm@gmail.com>
2023-09-08 12:36:02 +0300

    Правит нав, хедер, интро

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,20 @@
 //открывает-закрывает меню, но нет картинки бургера и крестика
 
-const nav = document.querySelector('.nav');
-const navBtn = document.querySelector('.nav__toggle');
 
-if (navBtn) {
-  navBtn.addEventListener('click', () => {
-    nav.classList.toggle('is-open');
-  })
+
+if (document.querySelector('.no-js')) {
+  document.querySelector('.no-js').classList.remove('no-js');
+}
+
+const menuBtn = document.querySelector('.nav__toggle');
+const menuNav = document.querySelector('.nav');
+
+
+if (menuBtn) {
+  menuBtn.addEventListener('click', function (evt) {
+    evt.preventDefault;
+    menuNav.classList.toggle('is-open');
+  });
 }
 
 
