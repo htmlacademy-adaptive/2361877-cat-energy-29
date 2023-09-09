6a9467c7c82a2bde59c3233f39c9c121a4795861
Julia Prokhorova <julia.pro.smm@gmail.com>
2023-09-09 16:02:40 +0300

    Поправлен js

--- source/js/script.js
+++ source/js/script.js
@@ -1,38 +1,12 @@
-//открывает-закрывает меню, но нет картинки бургера и крестика
-
-
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.nav__toggle');
 
 if (document.querySelector('.no-js')) {
   document.querySelector('.no-js').classList.remove('no-js');
 }
 
-const menuBtn = document.querySelector('.nav__toggle');
-const menuNav = document.querySelector('.nav');
-
-
-if (menuBtn) {
-  menuBtn.addEventListener('click', function (evt) {
-    evt.preventDefault;
-    menuNav.classList.toggle('is-open');
-  });
+if (navBtn) {
+  navBtn.addEventListener('click', () => {
+    nav.classList.toggle('is-open');
+  })
 }
-
-
-
-//код из обучающего видео, не работает
-
-//let navMain = document.querySelector('.nav');
-//let navToggle = document.querySelector('nav__toggle');
-
-//navMain.classList.remove('nav--nojs');
-
-//navToggle.addEventListener('click', function () {
- // if (navMain.classList.contains('nav__toggle--closed')) {
- //   navMain.classList.remove('nav__toggle--closed');
-  //  navMain.classList.add('nav__toggle--opened');
- // } else {
- //   navMain.classList.add('nav__toggle--closed');
-//    navMain.classList.remove('nav__toggle--opened');
-//  }
-//}
-//)
